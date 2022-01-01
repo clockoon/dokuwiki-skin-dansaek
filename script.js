a63d87bc43b45834f174e53db593938c869d92d2
Sungbin Jeon <clockoon@gmail.com>
2022-01-01 03:07:09 +0000

    modify layout (slightly)

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ jQuery( document ).ready( function( $ ) {
     $("main>section>div#dw__toc").insertAfter("main>section>h1");
     // move PageID under title
     $("main>div.pageId").insertAfter("main>section>h1");
-
+    // hide qsearch__out
+    $("header div#searchtools>form div#qsearch__out").hide();
 } );
